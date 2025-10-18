9e210fd009f613e078273065f86fd0ad2fcc4cd7
adspacheco <andersonac7@outlook.com>
2025-10-18 12:32:15 -0300

    add notification microservice

---
+++ notification/src/test/java/town/kairos/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package town.kairos.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
