302aef7a43cfaaac07d470bf95c612c02716aed2
adspacheco <andersonac7@outlook.com>
2025-10-18 22:13:45 -0300

    implementa logging

--- authuser/src/main/java/town/kairos/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/town/kairos/authuser/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package town.kairos.authuser.controllers;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,12 @@ import town.kairos.authuser.services.UserService;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+
 
     @Autowired
     UserService userService;
@@ -28,11 +31,14 @@ public class AuthenticationController {
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
                                                    @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
 
+        log.debug("POST registerUser userDto received {}", userDto.toString());
         if(userService.existsByUsername(userDto.getUsername())) {
+            log.warn("Username {} is already taken!", userDto.getUsername());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already taken!");
         }
 
         if(userService.existsByEmail(userDto.getEmail())) {
+            log.warn("Email {} is already taken!", userDto.getEmail());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already taken!");
         }
 
@@ -45,6 +51,19 @@ public class AuthenticationController {
         userModel.setLastUpdatedDate(LocalDateTime.now(ZoneId.of("UTC")));
         userService.save(userModel);
 
+        log.debug("POST registerUser userModel saved {}", userModel.toString());
+        log.info("User saved successfully userId {}", userModel.getUserId());
+
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
+        return "Logging Spring Boot...";
     }
 }
--- authuser/src/main/java/town/kairos/authuser/controllers/UserController.java
+++ authuser/src/main/java/town/kairos/authuser/controllers/UserController.java
@@ -1,6 +1,7 @@
 package town.kairos.authuser.controllers;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,7 @@ import java.time.ZoneId;
 import java.util.Optional;
 import java.util.UUID;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
@@ -61,12 +63,18 @@ public class UserController  {
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        log.debug("DELETE deleteUser userId received {}", userId);
+
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
 
         userService.delete(userModelOptional.get());
+
+        log.debug("DELETE deleteUser userModel userId {}", userId);
+        log.info("User deleted successfully userId {}", userId);
+
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
     }
 
@@ -74,6 +82,8 @@ public class UserController  {
     public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
                                              @Validated(UserDto.UserView.UserPut.class)
                                              @RequestBody @JsonView(UserDto.UserView.UserPut.class) UserDto userDto) {
+
+        log.debug("PUT updateUser userDto received {}", userDto.toString());
 
         Optional<UserModel> userModelOptional = userService.findById(userId);
 
@@ -88,6 +98,9 @@ public class UserController  {
         userModel.setLastUpdatedDate(LocalDateTime.now(ZoneId.of("UTC")));
 
         userService.save(userModel);
+
+        log.debug("PUT updateUser userModel saved {}", userModel.toString());
+        log.info("User updated successfully userId {}", userModel.getUserId());
 
         return ResponseEntity.status(HttpStatus.OK).body(userModel);
     }
@@ -104,6 +117,7 @@ public class UserController  {
         }
 
         if (!userModelOptional.get().getPassword().equals(userDto.getOldPassword())) {
+            log.warn("Mismatched old password userId {}", userDto.getUserId());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password!");
         }
 
@@ -113,6 +127,9 @@ public class UserController  {
 
         userService.save(userModel);
 
+        log.debug("PUT updatePassword userModel saved {} ", userModel.toString());
+        log.info("Password updated successfully userId {} ", userModel.getUserId());
+
         return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully.");
     }
 
@@ -120,6 +137,7 @@ public class UserController  {
     public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
                                               @RequestBody @Validated(UserDto.UserView.ImagePut.class)
                                               @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto) {
+        log.debug("PUT updateImage userDto received {} ", userDto.toString());
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
@@ -130,6 +148,9 @@ public class UserController  {
         userModel.setLastUpdatedDate(LocalDateTime.now(ZoneId.of("UTC")));
 
         userService.save(userModel);
+
+        log.debug("PUT updateImage userModel saved {} ", userModel.toString());
+        log.info("Image updated successfully userId {} ", userModel.getUserId());
 
         return ResponseEntity.status(HttpStatus.OK).body(userModel);
     }
