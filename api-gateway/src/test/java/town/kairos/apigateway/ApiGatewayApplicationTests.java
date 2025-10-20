60c1f053ca1b42ef00d9e546657becbc6ef8e241
adspacheco <andersonac7@outlook.com>
2025-10-20 19:17:28 -0300

    add `api-gateway` service and implements load balancer

---
+++ api-gateway/src/test/java/town/kairos/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package town.kairos.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
